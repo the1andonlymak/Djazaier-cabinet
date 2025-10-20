4c82e5a22cd6ec4136fcd630b90211eab857acc1
the1andonlymak <116656459+the1andonlymak@users.noreply.github.com>
2025-10-20 23:25:44 +0100

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oups ! Page introuvable</p>
-        <a href="/cryptoflow" className="text-blue-600 hover:text-blue-800 underline">
+        <a href="/" className="text-blue-600 hover:text-blue-800 underline">
           Retour à l’accueil
         </a>
       </div>
